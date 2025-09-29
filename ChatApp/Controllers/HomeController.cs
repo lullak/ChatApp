7b6ee83b9a4607f772ab73782f09e0b3bc3a74bf
Dennis <dennisbircan@gmail.com>
2025-09-29 13:02:42 +0200

    libs added

--- ChatApp/Controllers/HomeController.cs
+++ ChatApp/Controllers/HomeController.cs
@@ -8,25 +8,5 @@ namespace ChatApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
